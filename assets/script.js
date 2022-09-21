5afe3b712c10819876d4110154f6c3854b77f107
Andrew Min <minandrew0711@gmail.com>
2022-09-20 23:23:23 -0700

    connected api data with search results

--- assets/script.js
+++ assets/script.js
@@ -1,22 +1,16 @@
 var APIKey = "039a6b45ccd3001c8a73a24f6fa09a96";
 
 // example for lat and lon
-var city = "London";
-var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
-fetch(queryURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data){
-        console.log(data)
-    });
+var city = "";
 
 // search function 
-// 1. learn how to create search function
-// 2. create search history that saves a list underneath search bar
-// 2a create history through li for each city under ul(id=search-history)
-// 2aa create a way to cap search history to 10 
+// 1. learn how to create search function +
+// 2. create search history that saves a list underneath search bar +
+// 2a create history through li (id=historyresult) for each city under ul(id=search-history) +
+// 2aa create a way to cap search history to 10 +
 // 2ab add addEventListener to the li elements 
+// 3. only cities provide values. Make it so that search of an invalid thing provides no information/ gives an error 
+// 2aba might need to change li into a input and the ul into form elements 
 
 var searchSection = document.querySelector("#search");
 var searchHistory = document.querySelector(".search-history");
@@ -28,29 +22,43 @@ var historyArray = [];
 // provides value of search and saves the history in the search history section 
 function search() {
     console.log(searchResult.value);
-    searchHistory.children[0].remove();
     localStorage.setItem('searchhistory', searchResult.value);
     var history = localStorage.getItem('searchhistory');
     historyArray.push(history);
-    console.log(historyArray);
+    var historyresults = document.createElement('li');
+    historyresults.setAttribute("id", "historyResult " + searchResult.value);
+    historyresults.textContent = history;
+    searchHistory.append(historyresults);
 
-    if (historyArray.length === 4){
+    if (historyArray.length === 10) {
         historyArray.shift();
+        if (searchHistory.firstElementChild.innerHTML = history) {
+            searchHistory.firstElementChild.remove();
+        }
     };
-    console.log(historyArray);
-    for (var x in historyArray) {
-        var historyresults = document.createElement('li');
-        historyresults.setAttribute("id", "search-history");
-        historyresults.textContent = historyArray[x];
-        searchHistory.append(historyresults);
-    };
+
+    // value from search function will fetch city temperature
+    city = searchResult.value;
+    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+    fetch(queryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.list[0].main.temp);
+
+        });
+
+};
+
+function research() {
+    var x = this.innerHTML;
+    console.log(x);
 };
 
 // Gets the searchbtn to work with input
 searchbtn.addEventListener("click", search);
 
-
-// value from search function will fetch city temperature
 
 // displays current weather conditions (city name, date, weather icon,
 // temperature, humidity, // wind speed)
