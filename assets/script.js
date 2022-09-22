dd867a5987cd59c8336492b9535401fce2945fd0
Andrew Min <minandrew0711@gmail.com>
2022-09-22 09:14:38 -0700

    added js portion for jumbotron

--- assets/script.js
+++ assets/script.js
@@ -18,6 +18,12 @@ var searchbtn = document.querySelector("#searchbtn");
 var searchResult = document.querySelector("#search-form");
 var historyArray = [];
 
+var currentName = document.querySelector("#current-city");
+var currentTemp = document.querySelector("#temp");
+var currentWind = document.querySelector("#wind");
+var currentHumidity = document.querySelector("#humidity");
+var icon = document.querySelector("#icon");
+
 
 // provides value of search and saves the history in the search history section 
 function search() {
@@ -25,6 +31,8 @@ function search() {
     localStorage.setItem('searchhistory', searchResult.value);
     var history = localStorage.getItem('searchhistory');
     historyArray.push(history);
+
+    // creates the search history tabs
     var historyresults = document.createElement('li');
     historyresults.setAttribute("id", "historyResult " + searchResult.value);
     historyresults.textContent = history;
@@ -39,32 +47,52 @@ function search() {
 
     // value from search function will fetch city temperature
     city = searchResult.value;
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIKey;
     fetch(queryURL)
         .then(function (response) {
             return response.json();
         })
         .then(function (data) {
-            console.log(data.list[0].main.temp);
+            console.log(data)
+            // displays current weather conditions (city name, date, weather icon, temperature, humidity, // wind speed)
+            // 1. find which data equals each necessary requirement +
+            var currentCityName = data.name;
+            var currentCityTemp = data.main.temp;
+            var currentCityIcon = data.weather[0].icon;
+            var currentCityHumidity = data.main.humidity;
+            var currentCityWind = data.wind.speed;
+            var currentDate = Date(Date.UTC(data.dt));
+            var iconurl = "http://openweathermap.org/img/w/" + currentCityIcon + ".png";
+            icon.setAttribute("src", iconurl);
+            icon.style.display = "block";
 
+            currentName.textContent = currentCityName + " " + "(" + currentDate + ")";
+            currentTemp.textContent = "Temp: " + currentCityTemp + " °F";
+            currentWind.textContent = "Wind: " + currentCityWind + " MPH";
+            currentHumidity.textContent = "Humidity: " + currentCityHumidity + " %";
         });
 
+    var secondQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
+    fetch(secondQueryURL)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        });
+
+    function research() {
+        var x = this.innerHTML;
+        console.log(x);
+    };
 };
 
-function research() {
-    var x = this.innerHTML;
-    console.log(x);
-};
 
 // Gets the searchbtn to work with input
 searchbtn.addEventListener("click", search);
 
 
-// displays current weather conditions (city name, date, weather icon,
-// temperature, humidity, // wind speed)
-// 1. find which data equals each necessary requirement
-
 // displays cards with 5 day forecast of selected city 
 // find what is the 5 day forecast in the fetch
-// // create cards with li for each weather condition through javascript (id=card
+// create cards with li for each weather condition through javascript (id=card
 // for ul) all in div(id= cards-container)
